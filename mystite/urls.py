2550dd82870b3b85885c3c0218c0b2854658b372
stella-kim <stella@superstaracc.com>
2016-12-21 15:34:50 -0800

    Changed the HTML for the site.

--- mystite/urls.py
+++ mystite/urls.py
@@ -4,7 +4,8 @@ from django.contrib import admin
 urlpatterns = [
     # Examples:
     # url(r'^$', 'mystite.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    # url(r'^blog/', include('blog.urls')),    
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'',include('blog.urls')),
 ]
